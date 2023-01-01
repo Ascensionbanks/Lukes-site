f6c918de2cee31617c26af69f6fb0f79eab0f6aa
Ascensionbanks <78037864+Ascensionbanks@users.noreply.github.com>
2023-01-01 02:35:55 -0500

    donate

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function openCity(evt, cityName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+   PayPal.Donation.Button({
+       env: 'sandbox',
+       hosted_button_id: 'YOUR_SANDBOX_HOSTED_BUTTON_ID',
+       // business: 'YOUR_EMAIL_OR_PAYERID',
+       image: {
+           src: 'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
+           title: 'PayPal - The safer, easier way to pay online!',
+           alt: 'Donate with PayPal button'
+       },
+       onComplete: function (params) {
+           // Your onComplete handler
+       },
+   }).render('#paypal-donate-button-container');
